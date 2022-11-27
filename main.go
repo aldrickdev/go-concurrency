ffc463ac5636af19c2fafb54d1aeb928655a3331
aldrickdev <aldrickdev@gmail.com>
2022-11-27 16:28:13 +0000

    How to create a Generator

--- main.go
+++ main.go
@@ -7,11 +7,10 @@ import (
 )
 
 func main() {
-	// Creates a channel of type string and passes it to the boring go routine
-	c := make(chan string)
-	go boring("Boring!!!", c)
+	// Provides a channel of type string
+	c := boring("Boring!")
 
-	// Waits for a value to be received from the channel and prints it
+	// Pull from the channel 5 times
 	for i := 0; i < 5; i++ {
 		fmt.Printf("You say: %q\n", <-c)
 	}
@@ -19,10 +18,18 @@ func main() {
 	fmt.Println("You're boring, peace")
 }
 
-func boring(msg string, c chan string) {
-	// Sends message to the channel forever
-	for i := 0; ; i++ {
-		c <- fmt.Sprintf("%s %d", msg, i)
-		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-	}
+// Returns a READ ONLY channel
+func boring(msg string) <-chan string {
+	// Creates the channel
+	c := make(chan string)
+
+	// Launches a go routine that will place values into the channel
+	go func() {
+		// Sends message to the channel forever
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
 }
