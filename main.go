f5e187d36316c352adac37676d46ec3ec75ab932
aldrickdev <aldrickdev@gmail.com>
2022-11-28 01:57:30 +0000

    Select with timeout if a single message has a delay of over 1 second

--- main.go
+++ main.go
@@ -7,62 +7,33 @@ import (
 )
 
 func main() {
-	c := fanIn(boring("Joe"), boring("Ann"))
+	c := boring("Joe")
 
-	// Pull from the channels 5 times
-	for i := 0; i < 5; i++ {
-		// Gets the Message from the channel and prints the str
-		msg1 := <-c
-		fmt.Println(msg1.str)
+	for {
+		select {
+		case s := <-c:
+			fmt.Println(s)
 
-		msg2 := <-c
-		fmt.Println(msg2.str)
-
-		// Now that we got both messages, we can tell them to continue
-		msg1.wait <- true
-		msg2.wait <- true
+		// Creates a channel that will return a value when the time has elapsed
+		// Note that this timer will restart after every loop
+		case <-time.After(1 * time.Second):
+			fmt.Println("You're too slow")
+			return
+		}
 	}
-
-	fmt.Println("You're both boring, peace")
 }
 
 // Returns a READ ONLY channel
-func boring(msg string) <-chan Message {
-	// Creates the channels
-	c := make(chan Message)
-	waitForIt := make(chan bool)
-
-	// Launches a go routine that will place values into the channel
-	go func() {
-		// Sends message to the channel forever
-		for i := 0; ; i++ {
-			// Sends the message and a wait channeled used to wait for a signal to continue
-			c <- Message{fmt.Sprintf("%s: %d", msg, i), waitForIt}
-			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-			<-waitForIt
-		}
-	}()
-	return c
-}
-
-type Message struct {
-	str  string
-	wait chan bool
-}
-
-func fanIn(input1, input2 <-chan Message) <-chan Message {
-	c := make(chan Message)
+func boring(msg string) <-chan string {
+	rand.Seed(time.Now().UnixNano())
+	c := make(chan string)
 
 	go func() {
 		for {
-			select {
-			case s := <-input1:
-				c <- s
-			case s := <-input2:
-				c <- s
-			}
+			t := time.Duration(rand.Intn(2e3))
+			c <- fmt.Sprintf("%s\t%v", msg, t)
+			time.Sleep(t * time.Millisecond)
 		}
 	}()
-
 	return c
 }
